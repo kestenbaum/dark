6204525767ef22f788f960720a349b4e3e646b6e
kestenbaum <kespa71for@gmail.com>
2020-11-24 15:51:28 +0300

    'fixed1.1'

--- script/main.js
+++ script/main.js
@@ -1,4 +1,31 @@
+'use strict'
+
 $('.menu_btn').on('click', function (){
     $(this).toggleClass('menu_btn_active')
     $('.header_nav').toggleClass('header_active')
 })
+
+
+// slider
+
+let sliderItem = document.querySelectorAll('.slider_item'),
+    btnLeft = document.querySelector('.left_arrows'),
+    btnRight = document.querySelector('.right_arrows'),
+    itemResult = 0;
+
+btnRight.addEventListener('click', function (){
+    itemResult += 310
+    slide()
+})
+
+btnLeft.addEventListener('click', function (){
+    itemResult -= 310
+    slide()
+})
+
+
+function slide (){
+    for( let sliderResult of sliderItem){
+        sliderResult.style.transform = `translateX(${[itemResult]}px)`
+    }
+}
