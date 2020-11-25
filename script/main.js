b982288dc8f6ecb64de2a18b62f87874ee592765
kestenbaum <kespa71for@gmail.com>
2020-11-25 17:31:22 +0300

    'fixed12'

--- script/main.js
+++ script/main.js
@@ -1,31 +1,39 @@
 'use strict'
 
-$('.menu_btn').on('click', function (){
+$('.menu_btn').on('click', function () {
     $(this).toggleClass('menu_btn_active')
     $('.header_nav').toggleClass('header_active')
 })
-
 
 // slider
 
 let sliderItem = document.querySelectorAll('.slider_item'),
     btnLeft = document.querySelector('.left_arrows'),
     btnRight = document.querySelector('.right_arrows'),
-    itemResult = 0;
+    itemResult = 0,
+    num = 0
 
-btnRight.addEventListener('click', function (){
-    itemResult += 310
+btnRight.addEventListener('click', function () {
+
+    window.innerWidth >= 941 ? num = 4 : num = 2
+
+    Math.abs(itemResult / 100) > sliderItem.length - num ? itemResult = 0 : itemResult -= 100
+
     slide()
 })
 
-btnLeft.addEventListener('click', function (){
-    itemResult -= 310
+btnLeft.addEventListener('click', function () {
+
+    window.innerWidth >= 941 ? num = 3 : num = 1
+
+    itemResult === 0 ? itemResult = -((sliderItem.length - num) * 100) : itemResult += 100
+
     slide()
 })
 
 
-function slide (){
-    for( let sliderResult of sliderItem){
-        sliderResult.style.transform = `translateX(${[itemResult]}px)`
+function slide() {
+    for (let sliderResult of sliderItem) {
+        sliderResult.style.transform = `translateX(${[itemResult]}%)`
     }
 }
